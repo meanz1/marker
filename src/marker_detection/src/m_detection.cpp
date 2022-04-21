4c8e26a688cd48de3aa11f2136bc8babf9e5f731
ubuntu server <ynkim@cogaplex.com>
2022-04-21 17:34:00 +0900

    showing axes in marker

--- src/marker_detection/src/m_detection.cpp
+++ src/marker_detection/src/m_detection.cpp
@@ -2,115 +2,157 @@
 #include<opencv4/opencv2/aruco.hpp>
 #include<opencv4/opencv2/highgui.hpp>
 #include<opencv4/opencv2/opencv.hpp>
+#include<opencv4/opencv2/core/core.hpp>
+#include<opencv4/opencv2/imgproc/imgproc.hpp>
+#include"ros/ros.h"
+#include"std_msgs/String.h"
+#include<cv_bridge/cv_bridge.h>
+#include<image_transport/image_transport.h>
+#include<sensor_msgs/image_encodings.h>
+#include<sstream>
+#include<string>
+#include<fstream>
+#include<boost/algorithm/string.hpp>
 
-// int main(){
-//     std::cout << "m_detection" << std::endl;
+#include<jsoncpp/json/json.h>
 
-//     cv::Mat input_img = cv::imread("/home/minji/marker/src/marker_detection/src/marker.jpg");
-//     cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-//     cv::Mat markerImage;
-//     while(!input_img.empty())
-//     {
-        
-//         input_img.copyTo(markerImage);
-//         std::vector<int> ids;
-//         std::vector<std::vector<cv::Point2f> > corners;
-//         cv::aruco::detectMarkers(input_img, dictionary, corners, ids);
-
-//         if(ids.size() > 0)
-//         {
-//             cv::aruco::drawDetectedMarkers(markerImage, corners, ids);
-//         }
-//         cv::imshow("a", markerImage);
-//         cv::waitKey(0);
-//     }
-//     //cv::Mat a = cv::imread("/home/minji/marker/src/marker_detection/src/lenna.png");
-//     //cv::imshow("a", a);
-
-    
-//     // cv::aruco::drawMarker(dictionary, 61, 200, markerImage, 1);
-//     // cv::imwrite("/home/minji/marker/src/marker_detection/src/marker61.png", markerImage);
-
-    
-//     return 0;
-// }
-
-int main(int arc, char** arv)
-{
-    cv::VideoCapture cap(0);
-
-    if(!cap.isOpened())
+int main(int argc, char** argv) {
+    cv::VideoCapture inputVideo(0);
+    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    if(!inputVideo.isOpened())
     {
-        printf("Can't open the camera");
+        std::cout << "Error Opening video stream or file" << std::endl;
         return -1;
     }
+    
+    ros::init(argc, argv, "image_publisher");
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it(nh);
+    image_transport::Publisher pub = it.advertise("camera/image", 1);
+    std::string filename = "/home/cona/marker/src/marker_detection/src/cam_int";
+    std::ifstream readFile;
+    std::vector<std::string> yaml;
+    std::vector<int> rows;
+    std::vector<int> cols;
+    std::vector<std::string> data;
 
-    cv::Mat img;
+    std::vector<int> ids;
+        std::vector<std::vector<cv::Point2f>> corners;
+        std::vector<cv::Vec3d> rvecs, tvecs;
 
-    while(1)
+    cv::Mat frame, frame_cp;
+    cv::Mat camMatrix, distCoeffs;
+    std::string parser;
+
+    cv::FileStorage f_s(filename, cv::FileStorage::READ);
+
+    if(!f_s.isOpened())
+        std::cout<<"err"<<std::endl;
+    
+    f_s["camera_matrix"] >> camMatrix;
+    f_s["distortion_coefficients"] >> distCoeffs;
+
+    std::cout<<camMatrix<<std::endl;
+    
+    f_s.release();
+    
+    
+    while (1)
     {
-        cap >> img;
-        cv::imshow("camera+img", img);
+        inputVideo >> frame;
+        frame.copyTo(frame_cp);
+        if(frame.empty())
+            break;
+    
+        cv::Mat image = cv::imread(argv[0], cv::IMREAD_COLOR);
+        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
 
+        ros::Rate loop_rate(10);
+        if(nh.ok()) {
+            pub.publish(msg);
+            ros::spinOnce();
+            loop_rate.sleep();
+        }
+
+        cv::aruco::detectMarkers(frame, dictionary, corners, ids);
+
+        if(ids.size() > 0)
+            cv::aruco::drawDetectedMarkers(frame_cp, corners, ids);
+
+        cv::aruco::estimatePoseSingleMarkers(corners, 0.037, camMatrix, distCoeffs, rvecs, tvecs);
+        // std::cout << rvecs.size() << std::endl;
+        for (int i = 0; i < rvecs.size(); i ++)
+        {
+            std::cout << "rvecs[" << i << "] : "<< rvecs[i] << std::endl;
+        }
+
+        for (int i = 0; i < tvecs.size(); i ++)
+        {
+            std::cout << "tvecs[" << i << "] : "<< tvecs[i] << std::endl;
+        }
+
+        for (int i = 0; i < corners.size(); i++)
+        {
+            std::cout << "corners[" << i << "] : "<< corners[i] << std::endl;
+        }
+
+        for (int i=0; i<ids.size(); i++)
+        {
+            
+            cv::drawFrameAxes(frame_cp, camMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
+            
+        }
+
+        cv::Mat flip_frame_cp;
+        cv::flip(frame_cp, flip_frame_cp, 1);
+        cv::imshow("flip", flip_frame_cp);
+        cv::imshow("out", frame_cp);
         if(cv::waitKey(1)==27)
             break;
     }
-
     return 0;
 }
 
-// int main() {
-//     cv::VideoCapture inputVideo;
-//     inputVideo.open(0);
-//     cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-//     while (inputVideo.grab())
-//     {
-//         cv::Mat image, imageCopy;
-//         inputVideo.retrieve(image);
-//         image.copyTo(imageCopy);
+// class ImageConverter
+// {
+//     ros::NodeHandle nh_;
+//     image_transport::ImageTransport it_;
+//     image_transport::Subscriber image_sub_;
+//     image_transport::Publisher image_pub_;
 
-//         std::vector<int> ids;
-//         std::vector<std::vector<cv::Point2f>> corners;
-//         cv::aruco::detectMarkers(image, dictionary, corners, ids);
+//     public:
+//         ImageConverter()
+//             :it_(nh_)
+//         {
+//             image_sub_ = it_.subscribe("/camera/image_raw", 1, &ImageConverter::imageCb, this);
+//             image_pub_ = it_.advertise("/image_converter/output_video", 1);
+//         }
+        
+//         void imageCb(const sensor_msgs::ImageConstPtr& msg)
+//         {
+//             cv_bridge::CvImagePtr cv_ptr;
+//             try {
+//                 cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+//             }
+//             catch (cv_bridge::Exception& e) {
+//                 ROS_ERROR("cv_bridge exception:%s", e.what());
+//                 return;
+//             }
 
-//         if(ids.size() > 0)
-//             cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+//             if(cv_ptr->image.rows > 60 && cv_ptr -> image.cols > 60)
+//                 cv::circle(cv_ptr -> image, cv::Point(50, 50), 10, CV_RGB(255,0, 0));
 
-//         cv::imshow("out", imageCopy);
-//         cv::waitKey(0);
-//         return 0;
-//     }
-    
-// }
+//                 cv::imshow("d", cv_ptr->image);
+//                 cv::waitKey(0);
 
-// int main() {
-//     cv::VideoCapture inputVideo;
-//     cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-//     if(!inputVideo.isOpened())
-//     {
-//         std::cout << "Error Opening video stream or file" << std::endl;
-//         return -1;
-//     }
+//                 image_pub_.publish(cv_ptr->toImageMsg());
+//         }
+// };
 
-//     while (1)
-//     {
-//         cv::Mat frame, frame_cp;
-//         inputVideo >> frame;
-//         frame.copyTo(frame_cp);
-//         if(frame.empty())
-//             break;
-
-//         std::vector<int> ids;
-//         std::vector<std::vector<cv::Point2f>> corners;
-//         cv::aruco::detectMarkers(frame, dictionary, corners, ids);
-
-//         if(ids.size() > 0)
-//             cv::aruco::drawDetectedMarkers(frame_cp, corners, ids);
-
-//         cv::imshow("out", frame_cp);
-//         cv::waitKey(0);
-//         return 0;
-//     }
-
-    
+// int main(int argc, char** argv)
+// {
+//     ros::init(argc, argv, "image_converter");
+//     ImageConverter ic;
+//     ros::spin();
+//     return 0;
 // }
