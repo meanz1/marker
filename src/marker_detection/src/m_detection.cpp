64eae6ec28e9fed62cdc4afb7e2bb10693497b43
meanz1 <zndnfjqbd1206@gmail.com>
2022-06-07 13:57:51 +0900

    transpose

--- src/marker_detection/src/m_detection.cpp
+++ src/marker_detection/src/m_detection.cpp
@@ -15,27 +15,28 @@
 #include <boost/algorithm/string.hpp>
 #include <geometry_msgs/PoseArray.h>
 #include <geometry_msgs/Pose.h>
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2_ros/transform_broadcaster.h>
-#include <tf2/LinearMath/Vector3.h>
-#include <tf2/LinearMath/Transform.h>
-#include <tf2_msgs/TFMessage.h>
+#include <tf/LinearMath/Quaternion.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include <tf/LinearMath/Vector3.h>
+#include <tf/LinearMath/Transform.h>
+// #include <tfMessage/TFMessage.h>
 #include <tf/transform_datatypes.h>
 #include <deque>
 
 cv::Mat frame;
 cv::Mat frame_cp;
 bool flag = false;
-tf2::Quaternion q;
-tf2::Quaternion q_;
+tf::Quaternion q;
+tf::Quaternion q_;
 // marker2cam (matrix)
 
-tf2::Vector3 cv_vector3d_to_tf_vector3_m(const cv::Mat &vec)
+tf::Vector3 cv_vector3d_to_tf_vector3_m(const cv::Mat &vec)
 {
     return {vec.at<double>(0, 0), vec.at<double>(1, 0), vec.at<double>(2, 0)};
 }
 
-tf2::Quaternion cv_vector3d_to_tf_quaternion_m(const cv::Mat &rvec)
+tf::Quaternion cv_vector3d_to_tf_quaternion_m(const cv::Mat &rvec)
 {
     cv::Mat rotation_mat;
     auto ax = rvec.at<double>(0, 0), ay = rvec.at<double>(1, 0), az = rvec.at<double>(2, 0);
@@ -51,9 +52,9 @@ tf2::Quaternion cv_vector3d_to_tf_quaternion_m(const cv::Mat &rvec)
     return q;
 }
 
-tf2::Transform create_transform_m(const cv::Mat &tvec, const cv::Mat &rvec)
+tf::Transform create_transform_m(const cv::Mat &tvec, const cv::Mat &rvec)
 {
-    tf2::Transform transform;
+    tf::Transform transform;
     transform.setOrigin(cv_vector3d_to_tf_vector3_m(tvec));
     transform.setRotation(cv_vector3d_to_tf_quaternion_m(rvec));
     return transform;
@@ -61,12 +62,12 @@ tf2::Transform create_transform_m(const cv::Mat &tvec, const cv::Mat &rvec)
 
 // cam2marker (cv::Vec ver)
 
-tf2::Vector3 cv_vector3d_to_tf_vector3_v(const cv::Vec3d &vec)
+tf::Vector3 cv_vector3d_to_tf_vector3_v(const cv::Vec3d &vec)
 {
     return {vec[0], vec[1], vec[2]};
 }
 
-tf2::Quaternion cv_vector3d_to_tf_quaternion_v(const cv::Vec3d &rvec)
+tf::Quaternion cv_vector3d_to_tf_quaternion_v(const cv::Vec3d &rvec)
 {
     cv::Mat rotation_mat;
     auto ax = rvec[0], ay = rvec[1], az = rvec[2];
@@ -77,14 +78,14 @@ tf2::Quaternion cv_vector3d_to_tf_quaternion_v(const cv::Vec3d &rvec)
     auto qy = ay * sina / angle;
     auto qz = az * sina / angle;
     auto qw = cosa;
-    
+
     q_.setValue(qx, qy, qz, qw);
     return q_;
 }
 
-tf2::Transform create_transform_v(const cv::Vec3d &tvec, const cv::Vec3d &rvec)
+tf::Transform create_transform_v(const cv::Vec3d &tvec, const cv::Vec3d &rvec)
 {
-    tf2::Transform transform;
+    tf::Transform transform;
     transform.setOrigin(cv_vector3d_to_tf_vector3_v(tvec));
     transform.setRotation(cv_vector3d_to_tf_quaternion_v(rvec));
     return transform;
@@ -117,7 +118,7 @@ int main(int argc, char **argv)
     // cv::VideoCapture inputVideo(0);
 
     cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-    cv::Ptr<cv::aruco::GridBoard> board = cv::aruco::GridBoard::create(5, 7, 0.1485, 0.0135, dictionary); 
+    cv::Ptr<cv::aruco::GridBoard> board = cv::aruco::GridBoard::create(5, 7, 0.1485, 0.0135, dictionary);
 
     // for (int i = 0; i < 35; i++)
     // {
@@ -140,7 +141,7 @@ int main(int argc, char **argv)
 
     std::string filename = "/home/cona/marker/src/marker_detection/src/cam_int";
 
-    ros::Publisher tf_list_pub_ = nh.advertise<tf2_msgs::TFMessage>("/tf", 10);
+    // ros::Publisher tf_list_pub_ = nh.advertise<tf_msgs::TFMessage>("/tf", 10);
     std::deque<double> dqx;
     std::deque<double> dqy;
     std::deque<double> dqz;
@@ -161,11 +162,11 @@ int main(int argc, char **argv)
 
     cv::Mat camMatrix, distCoeffs;
 
-    tf2::Transform transform;
+    tf::Transform transform;
     cv::Mat R;
 
-    tf2_ros::TransformBroadcaster br;
-    tf2_msgs::TFMessage tf_msg_list_;
+    tf::TransformBroadcaster br;
+    // tf_msgs::TFMessage tf_msg_list_;
 
     cv::FileStorage f_s(filename, cv::FileStorage::READ);
 
@@ -184,6 +185,8 @@ int main(int argc, char **argv)
     // while (ros::ok())
     // {
     image_transport::Subscriber sub = it.subscribe("/camera/rgb/image_raw", 1, imagecallback);
+    tf::StampedTransform tf_b2c;
+    tf::TransformListener listener;
     ros::Rate loop_rate(10);
 
     while (ros::ok())
@@ -191,10 +194,10 @@ int main(int argc, char **argv)
         if (flag == true)
         {
             cv::aruco::detectMarkers(frame, dictionary, corners, ids);
-            
+
             for (int i = 0; i < ids.size(); i++)
             {
-                for (int j = 0; j < 4; j ++)
+                for (int j = 0; j < 4; j++)
                 {
                     std::cout << "[" << i << "] : " << corners[i][j] << std::endl;
                 }
@@ -244,17 +247,18 @@ int main(int argc, char **argv)
             // marker2cam
 
             auto transform_m = create_transform_m(T_inv, rvecs_inv); // m2c
-            geometry_msgs::TransformStamped tf_msg_m;
-            tf_msg_m.header.stamp = ros::Time::now();
-            tf_msg_m.header.frame_id = "marker";
-            std::stringstream mss;
-            mss << marker_tf_prefix2 << "cam";
-            tf_msg_m.child_frame_id = mss.str();
+            // geometry_msgs::TransformStamped tf_msg_m;
+            tf::Transform tf_msg_m;
+            // tf_msg_m.header.stamp = ros::Time::now();
+            // tf_msg_m.header.frame_id = "marker";
+            // std::stringstream mss;
+            // mss << marker_tf_prefix2 << "cam";
+            // tf_msg_m.child_frame_id = mss.str();
 
-            tf_msg_m.transform.translation.x = transform_m.getOrigin().getX();
-            tf_msg_m.transform.translation.y = transform_m.getOrigin().getY();
-            tf_msg_m.transform.translation.z = transform_m.getOrigin().getZ();
-
+            // tf_msg_m.transform.translation.x = transform_m.getOrigin().getX();
+            // tf_msg_m.transform.translation.y = transform_m.getOrigin().getY();
+            // tf_msg_m.transform.translation.z = transform_m.getOrigin().getZ();
+            tf_msg_m.setOrigin(tf::Vector3(transform_m.getOrigin().getX(), transform_m.getOrigin().getY(), transform_m.getOrigin().getZ()));
             if (dqz.size() >= 10)
             {
                 dqz.pop_front();
@@ -270,9 +274,9 @@ int main(int argc, char **argv)
                 dqy.pop_front();
             }
 
-            dqx.push_back(tf_msg_m.transform.translation.x);
-            dqy.push_back(tf_msg_m.transform.translation.y);
-            dqz.push_back(tf_msg_m.transform.translation.z);
+            dqx.push_back(tf_msg_m.getOrigin().getX());
+            dqy.push_back(tf_msg_m.getOrigin().getY());
+            dqz.push_back(tf_msg_m.getOrigin().getZ());
 
             double dqx_sum = 0;
             double dqy_sum = 0;
@@ -296,8 +300,6 @@ int main(int argc, char **argv)
             }
             dqy_ave = dqy_sum / dqy.size();
 
-    
-    
             std::cout << "x : " << transform_m.getOrigin().getX() << " y : " << transform_m.getOrigin().getY() << " z : " << transform_m.getOrigin().getZ() << std::endl;
             std::cout << "dqx ave : " << dqx_ave << std::endl;
             std::cout << "dqx size : " << dqx.size() << std::endl;
@@ -306,11 +308,14 @@ int main(int argc, char **argv)
             std::cout << "dqz ave : " << dqz_ave << std::endl;
             std::cout << "dqz size : " << dqz.size() << std::endl;
 
-            tf_msg_m.transform.rotation.x = transform_m.getRotation().getX();
-            tf_msg_m.transform.rotation.y = transform_m.getRotation().getY();
-            tf_msg_m.transform.rotation.z = transform_m.getRotation().getZ();
-            tf_msg_m.transform.rotation.w = transform_m.getRotation().getW();
-
+            // tf_msg_m.transform.rotation.x = transform_m.getRotation().getX();
+            // tf_msg_m.transform.rotation.y = transform_m.getRotation().getY();
+            // tf_msg_m.transform.rotation.z = transform_m.getRotation().getZ();
+            // tf_msg_m.transform.rotation.w = transform_m.getRotation().getW();
+            tf_msg_m.setRotation(tf::Quaternion(transform_m.getRotation().getX(), transform_m.getRotation().getY(), transform_m.getRotation().getZ(), transform_m.getRotation().getW()));
+            br.sendTransform(tf_msg_v);
+            tf::StampedTransform tf_st_msg_m(tf_msg_m, ros::Time::now(), "marker", "cam");
+            br.sendTransform(tf_st_msg_m);
             if (dqx.size() == 10)
             {
                 std::ofstream value_text;
@@ -331,9 +336,18 @@ int main(int argc, char **argv)
                 // double siny_cosp = 2 * (tf_msg_v.transform.rotation.w * tf_msg_v.transform.rotation.z + tf_msg_v.transform.rotation.x * tf_msg_v.transform.rotation.y);
                 // double cosy_cosp = 1 - 2 * (tf_msg_v.transform.rotation.y * tf_msg_v.transform.rotation.y + tf_msg_v.transform.rotation.z * tf_msg_v.transform.rotation.z);
                 // double yaw = std::atan2(siny_cosp, cosy_cosp);
-                
-                tf2::Matrix3x3 m(q_);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
-                double roll=0.0, pitch=0.0, yaw=0.0;
+                bool output = false;
+                bool scs = listener.waitForTransform("/cam", "/base_link", ros::Time(0), ros::Duration(1.0));
+                if (!scs)
+                    return output;
+                listener.lookupTransform("/cam", "/base_link", ros::Time(0), tf_st_msg_m);
+
+                // br.sendTransform(tf_b2c);
+
+                tf::Quaternion value_rpy;
+                value_rpy.setValue(tf_msg_v.transform.rotation.x, tf_msg_v.transform.rotation.y, tf_msg_v.transform.rotation.z, tf_msg_v.transform.rotation.w);
+                tf::Matrix3x3 m(value_rpy);
+                double roll = 0.0, pitch = 0.0, yaw = 0.0;
                 m.getRPY(roll, pitch, yaw);
 
                 std::string str_r = " ROTATION r : " + std::to_string(roll) + " p : " + std::to_string(pitch) + " y : " + std::to_string(yaw);
@@ -344,9 +358,9 @@ int main(int argc, char **argv)
             }
 
             // broadcast tf_msg
-            br.sendTransform(tf_msg_v);
-            br.sendTransform(tf_msg_m);
-
+            // br.sendTransform(tf_msg_v);
+            // br.sendTransform(tf_msg_m);
+            // br.sendTransform(tf::StampedTransform(tf_msg_m, ros::Time::now(), "marker", "cam"));
             cv::circle(frame_cp, cv::Point(344, 216), 10, cv::Scalar(255, 0, 0));
             flag = false;
             cv::imshow("out", frame_cp);
